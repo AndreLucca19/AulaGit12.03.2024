ff44fa4bda4fca856fdb50335c52cad7a2421e09
André Lucca <fonsecasolnge@gmail.com>
2024-03-12 08:21:56 -0300

    Primeiro Commit

---
+++ sad.js
@@ -0,0 +1,17 @@
+let inicio;
+let fim;
+let a = 20;
+let b = 30;
+
+if (a < b) {
+    inicio = a;
+    fim = b;
+} else {
+    inicio = b;
+    fim = a;
+}
+
+while (inicio <= fim) {
+    console.log(inicio);
+    inicio++;
+}
